0dfb4ec9401dec31716835fb1fc83cf6b1aeacf1
thelonesomeprogrammer <m@thelonesomeprogrammer.com>
2024-10-25 14:33:37 +0200

    samll fixes

--- datasort.py
+++ datasort.py
@@ -3,19 +3,23 @@ import csv
 
 ## i,height,width,GPS Latitude,GPS Longitude,GPS Altitude,Roll,Pitch,Yaw
 ## load the annotations
-def load_csv_to_arr(csv):
-    with open() as csv_file:
+def load_csv_to_arr(file):
+    with open(file) as csv_file:
+        anno = []
         csv_reader = csv.reader(csv_file, delimiter=',')
         for k, row in enumerate(csv_reader):
-        if k != 0:
-            unsorted.append(list(map(float, row[0:])))
+            if k != 0:
+                anno.append(list(map(float, row[0:])))
+        return anno
 
-def tresh():
-    all = len(unsorted)
+def tresh_md_table(unfiltered):
+    all = len(unfiltered)
     last = 0
+    print("e|e|e")
+    print("|--|--|--|")
     for j in range(60):
         rm = 0
-        for i in unsorted:
+        for i in unfiltered:
             if abs(i[6])>j or abs(i[7])>j:
                 rm += 1
         print(str(j)+"|"+str(all - rm)+"|"+str(last-rm))
@@ -23,5 +27,8 @@ def tresh():
 
 
 def main():
-    load_csv_to_arr('./Billed Data/GNSS_data.csv')
+    data = load_csv_to_arr('./Billed Data/GNSS_data.csv')
+    tresh_md_table(data)
 
+if __name__ == "__main__":
+    main()
