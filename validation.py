d3784e6c53994e7d7ca04f127905d5b189c42ea3
thelonesomeprogrammer <m@thelonesomeprogrammer.com>
2024-10-29 09:48:22 +0100

    avg hit frequency

--- validation.py
+++ validation.py
@@ -2,10 +2,7 @@ import math
 import numpy as np
 import csv
 
-r= 6371000
-
-# [id, x, y]
-
+## file: path to annotation csv
 def load_csv_to_arr(file):
     with open(file) as csv_file:
         anno = []
@@ -15,12 +12,8 @@ def load_csv_to_arr(file):
                 anno.append(list(map(float, row[0:])))
         return anno
 
-data = load_csv_to_arr('./Billed Data/GNSS_data.csv')
 
-fake_Pred = [[data[0][0],data[0][3],data[0][4]]]
-fake_Target = [[data[0][0],data[3][3],data[3][4]]]
-
-
+# Pred: [[id, x, y]], Target: [[id, x, y]] 
 def cal_dist(Pred, Target):
     dist_list=[]
     for i,v in enumerate(Pred):
@@ -31,22 +24,45 @@ def cal_dist(Pred, Target):
             lon2 = math.radians(Target[i][2])
             dlat = lat2 - lat1
             dlon = lon2 - lon1
-            a = math.sin(dlat/2)**2 + math.cos(lat1)*math.cos(lat2)*math.sin(dlon/2)**2
-            c = 2* math.atan2(math.sqrt(a), math.sqrt(1-a))
+            a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+            c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
-            Dist = r*c
+            Dist = 6371000 * c
             dist_list.append(Dist)
     return dist_list
 
             
+# Pred: [[id, x, y]]
+def validation(Pred, Targets):
+    pred_list = np.array(sorted(np.asarray(Pred), key=lambda x: x[0]))
+    startsearch = 0
+    hit_deltas = []
+    last_hit = 0
+    Target = []
+    for v in pred_list:
+        for i in range(startsearch, len(Targets)):
+            startsearch += 1
+            if Targets[i][0] == v[0]:
+                Target.append([data[i][0],data[i][3],data[i][4]])
+                hit_deltas.append(startsearch - last_hit - 1)
+                last_hit = startsearch - 1
+                break
 
-def validation(Pred, Target):
     dist_list = np.asanyarray(cal_dist(Pred, Target))
-    succeslist = np.where(dist_list<=50)
-    succesrate = len(succeslist)/len(dist_list)*100
+    succeslist = np.where(dist_list <= 50)[0]
+    print(dist_list)
+    print(succeslist)
+    succesrate = len(succeslist) / len(dist_list) * 100
     meanerror = dist_list.mean()
+    avg_hit = np.asanyarray(hit_deltas).mean()
+    print(avg_hit)
     print(succesrate)
     print(meanerror)
 
 
-validation(fake_Pred,fake_Target)
+data = load_csv_to_arr('./Billed Data/GNSS_data.csv')
+
+fake_Pred = [[data[3][0],data[0][3],data[0][4]], [data[6][0],data[3][3],data[3][4]]]
+
+
+validation(fake_Pred,data)
