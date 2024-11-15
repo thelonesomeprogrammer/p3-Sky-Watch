09a63bab7b2e8a83ba88de66dbb98c60f811a4b0
thelonesomeprogrammer <m@thelonesomeprogrammer.com>
2024-11-15 10:09:23 +0100

    vaipr 100

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from pnp import sky_vores_test
 
 
 def main(data_path,max_keypoints):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     extractor = SuperPoint(max_num_keypoints=max_keypoints).eval().to(device)
     sat_extractor = SuperPoint(max_num_keypoints=max_keypoints).eval().to(device)
     matcher = LightGlue(features="superpoint").eval().to(device)
@@ -32,7 +32,11 @@ def main(data_path,max_keypoints):
     for i in range(5):
         for j in range(5):
             tile = sat_img[:, i*fraci:(i+1)*fraci, j*fracj:(j+1)*fracj]
-            sat_features.append(sat_extractor.extract(tile.unsqueeze(0)))
+            feature = sat_extractor.extract(tile.unsqueeze(0))
+            feature["keypoints"][0][:, 0] += i*fraci
+            feature["keypoints"][0][:, 1] += j*fracj
+            sat_features.append(feature)
+            
     bounds = load_bonuds(data_path+"SatData/boundaries.txt")
     target = []
     imgs = []
@@ -45,17 +49,13 @@ def main(data_path,max_keypoints):
         target.append([i[0],i[1],i[2]])
         imgs.append([i[0],img])
         img_features = extractor.extract(img.unsqueeze(0))
-        features.append([i[0],rbd(img_features)])
-        img_matches = matcher({"image0": sat_features, "image1": img_features})
-        matches.append([i[0],img_matches["matches0"],img_matches["matches1"],
-                            img_matches["matching_scores0"],img_matches["matching_scores1"],
-                        img_matches["matches"],img_matches["scores"]])
-        sat_keypoints = np.asarray([[
-            sat_features["keypoints"][0][int(t)][0],
-            sat_features["keypoints"][0][int(t)][1],
-            int(t)
-            ] for t in img_matches["matches0"][0] if not torch.all(t == -1)])
-        db = DBSCAN(eps=500, min_samples = 2).fit(sat_keypoints[:,:2])
+        all_keypoints = np.empty((0,3))
+        for i in sat_features:
+            img_matches = matcher({"image0": i, "image1": img_features})
+            print(img_matches)
+            sat_keypoints = np.asarray([[i["keypoints"][0][int(t)][0], i["keypoints"][0][int(t)][1],int(t)] for t in img_matches["matches1"][0] if not torch.all(t == -1)])
+            all_keypoints = np.concatenate((all_keypoints, sat_keypoints), axis=0)
+        db = DBSCAN(eps=500, min_samples = 2).fit(all_keypoints[:,:2])
         labels = db.labels_
 
 
@@ -67,7 +67,7 @@ def main(data_path,max_keypoints):
         for label in unique_labels:
                 # Color for each cluster; noise points are in black
             color = colors(label) if label != -1 else (0, 0, 0, 1)
-            plt.scatter(sat_keypoints[:,:2][labels == label, 0], sat_keypoints[:,:2][labels == label, 1], c=[color], label=f"Cluster {label}" if label != -1 else "Noise")
+            plt.scatter(all_keypoints[:,:2][labels == label, 0], all_keypoints[:,:2][labels == label, 1], c=[color], label=f"Cluster {label}" if label != -1 else "Noise")
 
         plt.title("DBSCAN Clustering of Points")
         plt.xlabel("X Coordinate")
